475ff97cd76c753df87680a819b76b31d9fc8ba8
Ričardas Pocius <cirka@cirka.lt>
2016-11-18 15:37:39 +0200

    Initial release

---
+++ src/ldapregister_app.erl
@@ -0,0 +1,16 @@
+-module(ldapregister_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ldapregister_sup:start_link().
+
+stop(_State) ->
+    ok.
