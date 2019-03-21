ab0739f6ad76c30e635b7ed7c4a07abb7fb2504d
Ričardas Pocius <cirka@cirka.lt>
2019-03-21 15:17:53 +0200

    start using rebar3 instead of old rebar

--- debian/make_deb.sh
+++ debian/make_deb.sh
@@ -2,22 +2,24 @@
 rm ldapregister_*.deb
 rm -rf deb
 rm -rf rel/ldapregister
+rm -rf _build
 set -e
-rebar get-deps compile generate
+rebar3 release
 mkdir -p deb/root/opt/ldapregister/ssl
 mkdir -p deb/root/etc/systemd/system
-mv rel/ldapregister/* deb/root/opt/ldapregister
+mv _build/default/rel/ldapregister/* deb/root/opt/ldapregister
 cat ./debian/ldapregister.config > deb/root/opt/ldapregister/ldapregister.config
 cat ./debian/ldapregister.service > deb/root/etc/systemd/system/ldapregister.service
 chmod 644 deb/root/etc/systemd/system/ldapregister.service
 fpm -s dir -t deb \
+  --deb-no-default-config-files \
   -n "ldapregister" \
   --deb-user root \
   --deb-group root \
   --config-files /opt/ldapregister/ldapregister.config \
   --after-install debian/post_install.sh \
   -a all \
-  -v 0.0.4 \
+  -v 0.0.5 \
   -m "Ricardas Pocius <cirka@cirka.lt>" \
   --description "Website for registration to Technarium LDAP" \
   -C ./deb/root opt etc
